60bf4162517d37a6c3b517ebc46813ef85aa0b93
BOTLOCK <gustavo.olier27@gmail.com>
2024-01-31 23:46:13 -0300

    second commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection;
+namespace DotnetLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            //var meuArray = new Teste[2] { new Teste(), new Teste() };
+            var meuArray = new string[2] { "ola", "teste" };
+
+            foreach (var item in meuArray)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            var funcionarios = new Funcionario[5];
+            funcionarios[0] = new Funcionario() { Id = 10, Name = "Gustavo" };
+
+            foreach (var funcionario in funcionarios)
+            {
+                Console.WriteLine(funcionario.Id);
+                Console.WriteLine(funcionario.Name);
+            }
+
+        }
+
+        public struct Funcionario
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+
+        }
+
+
+    }
+}
